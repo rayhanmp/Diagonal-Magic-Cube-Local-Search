28d48e303e03fc322c4c7a578fa3c4774195846c
jasminecallista <124756117+jasminecallista@users.noreply.github.com>
2024-11-04 22:46:47 +0700

    Update simulated-annealing.py

--- src/simulated-annealing.py
+++ src/simulated-annealing.py
@@ -1,1 +1,44 @@
-# TBD
+from magic_utils import objective_function
+import numpy as np
+import math
+import random
+
+# Implementasi Algoritma Simulated Annealing
+def simulated_annealing(cube, magicNumber, temperature, cooling_rate, iterations, threshold):
+    n = cube.shape[0]
+    current_cube = cube.copy()
+    current_score = objective_function(current_cube, magicNumber)
+    best_cube = current_cube.copy()
+    best_score = current_score
+
+    for _ in range(iterations):
+        # Pilih dua posisi acak untuk ditukar
+        i, j, k = np.random.randint(0, n, size=3)
+        x, y, z = np.random.randint(0, n, size=3)
+
+        # Tukar elemen terpilih
+        current_cube[i, j, k], current_cube[x, y, z] = current_cube[x, y, z], current_cube[i, j, k]
+
+        # Hitung nilai objective function baru
+        new_score = objective_function(current_cube, magicNumber)
+
+        # Hitung delta E dan probabilitas
+        delta_E = current_score - new_score
+      
+        # Metropolis acceptance criterion
+        probability = math.exp(min(max(delta_E / temperature, -700), 700)) # Dibatasi untuk rentang [-700, 700] untuk menghidari overflow error
+
+        # Tentukan apakah menerima state baru
+        if delta_E > 0 or probability > threshold:
+            current_score = new_score
+            if new_score < best_score:
+                best_cube = current_cube.copy()
+                best_score = new_score
+        else:
+            # Kembalikan ke kondisi sebelumnya jika tidak diterima
+            current_cube[i, j, k], current_cube[x, y, z] = current_cube[x, y, z], current_cube[i, j, k]
+
+        # Kurangi temperatur
+        temperature *= cooling_rate
+
+    return best_cube, best_score
